80c97276733d88465086147ce1a3e0423afe866f
Noah Kattner <gasdrop14@gmail.com>
2025-04-24 13:02:51 -0400

    Created a function that will plot a circle and display it

--- Lab15_nkattner_1.py
+++ Lab15_nkattner_1.py
@@ -14,8 +14,19 @@ class Main:
         self.x_values = [math.cos(math.radians(angle)) for angle in self.angles]
         self.y_values = [math.sin(math.radians(angle)) for angle in self.angles]
 
-        # graph_circle()
+        self.graph_circle()
 
+
+    def graph_circle(self):
+        plot.figure(figsize=(15, 8))
+        plot.plot(self.x_values, self.y_values, label = "Circle", linewidth = 2, linestyle = "-", color = "blue")
+        plot.title("Circumference of a Circle")
+        plot.xlabel("Cosine")
+        plot.ylabel("Sine")
+        plot.grid(True)
+        plot.axis('equal')
+        plot.legend()
+        plot.show()
 
 
 if __name__ == '__main__':
