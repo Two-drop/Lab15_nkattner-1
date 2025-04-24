72382687cbb3e0aa0b16d116ec9b788489306d6c
Noah Kattner <gasdrop14@gmail.com>
2025-04-24 12:47:37 -0400

    Updated the requirements and created the main class that will run the program with x and y values for a circle

--- Lab15_nkattner_1.py
+++ Lab15_nkattner_1.py
@@ -1,5 +1,22 @@
 """
-Graoh Plotter
+Graph Plotter
 author: Noah Kattner
 date: 4/24/2025
 """
+
+import math
+import matplotlib.pyplot as plot
+
+
+class Main:
+    def __init__(self):
+        self.angles = range(360)
+        self.x_values = [math.cos(math.radians(angle)) for angle in self.angles]
+        self.y_values = [math.sin(math.radians(angle)) for angle in self.angles]
+
+        # graph_circle()
+
+
+
+if __name__ == '__main__':
+    Main()
